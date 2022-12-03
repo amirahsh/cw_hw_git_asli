41f09cee190f2adc3a741a4d61a5082ab1d2f83e
amirahsh <amirhoseinshahidi20041382@gmail.com>
2022-12-03 11:38:44 +0330

    dar branch jadid  dar bar dovom tamami tavabe pak shod

--- main.c
+++ main.c
@@ -1,48 +1,7 @@
-#include <stdio.h>
+
 #include <stdio.h>
 #include<math.h>
-int begintoend(int begin,int end,int number){
-    //int i=1;
-   // int sum=0;
-    //while( number > 0) {
-     //   sum=sum+(number%10)*i;
-      //  number=number/10;
-      //  i=i*begin;
 
-   //    1-code ta inja comment shod
-    }
-    /*
-    i=0;
-    int new=0;
-    while(sum!=0){
-        new=new+(sum%end)*pow(10,i);
-        i++;
-        sum=sum/end;
-    }
-    return new;
-    */
-    //   2- chand khat digar comment shod
-}
-/*
-int numberofdigits(int ournum){
-    int i2=0;
-    while(ournum!=0){
-        ournum=ournum/10;
-        i2=i2+1;
-    }
-    return i2;
-}
-int numbertobase10(int beginbase,int number){
-    int sum=0;
-    int i=1;
-    while( number > 0) {
-        sum=sum+(number%10)*i;
-        number=number/10;
-        i=i*beginbase;
-    }
-    return sum;
-}
-*/
 int main(){
     int number ,begin ,end ,sum;
     scanf("%d %d %d",&begin,&end,&number);
