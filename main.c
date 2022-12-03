965dc78b6e48b224d4a2b2ba8638e0ee52b0f2e0
amirahsh <amirhoseinshahidi20041382@gmail.com>
2022-12-03 11:43:23 +0330

    dar branch jadid  dar bar dovom tamami tavabe pak shod

--- main.c
+++ main.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 #include<math.h>
 int begintoend(int begin,int end,int number){
-    //int i=1;
-   // int sum=0;
-    //while( number > 0) {
-     //   sum=sum+(number%10)*i;
-      //  number=number/10;
-      //  i=i*begin;
+    int i=1;
+    int sum=0;
+    while( number > 0) {
+        sum=sum+(number%10)*i;
+        number=number/10;
+        i=i*begin;
 
-   //    1-code ta inja comment shod
+       1-code ta inja comment shod
     }
-    /*
+
     i=0;
     int new=0;
     while(sum!=0){
@@ -20,8 +20,7 @@ int begintoend(int begin,int end,int number){
         sum=sum/end;
     }
     return new;
-    */
-    //   2- chand khat digar comment shod
+    
 }
 int numberofdigits(int ournum){
     int i2=0;
@@ -42,26 +41,5 @@ int numbertobase10(int beginbase,int number){
     return sum;
 }
 
-int main(){
-    int number ,begin ,end ,sum;
-    scanf("%d %d %d",&begin,&end,&number);
-    int newnum= begintoend(begin,end,number);
-    int i2= numberofdigits(newnum);
-    //printf("%d\t",num);
-    if(i2%2==1){
-        newnum=newnum*10;
-        i2=i2+1;
-    }
-    int x=pow(10,i2/2);
-    int y=pow(10,i2);
-    int firstpart=(newnum%x);
-    int secondpart=((newnum%y)-firstpart)/x;
-    int final1= numbertobase10(end,firstpart);
-    int final2= numbertobase10(end,secondpart);
-    int final= begintoend(10,end,final1+final2);
-    printf("%d",final);
-   // printf("%d\t%d\t%d\t%d\t%d\t%d\t%d",final,final1,final2,firstpart,secondpart,newnum,i2);
 
-return 0;
-}
 
